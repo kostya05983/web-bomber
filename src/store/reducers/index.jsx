1096aa01e399e3cab5082d5b40b0b2b5932cd04f
kubitre <kubitre@gmail.com>
2019-05-02 15:02:45 +0700

    [ADD] Create global store

---
+++ src/store/reducers/index.jsx
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+
+// Navigation part
+import NS_navigationState from '../NavigationPart/reducer';
+
+
+export default combineReducers({
+    NS_navigationState,
+})
