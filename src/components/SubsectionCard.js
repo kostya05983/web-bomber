e49a516c22f9d56e8a045ebdedd6fa2b7cf647a2
kubitre <kubitre@gmail.com>
2019-05-02 15:01:58 +0700

    [REF] Rebase Routers, fix syntax

--- src/components/SubsectionCard.js
+++ src/components/SubsectionCard.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import {Button, Card} from "react-bootstrap";
+import React from 'react';
+// import { Link } from 'react-router-dom'
+import { Button, Card } from "react-bootstrap";
 
 
-export default class SubsectionCard extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Card border="primary" className="subsection">
-                <Card.Body>
-                    <Card.Title>{this.props.title}</Card.Title>
-                    <Card.Text>{this.props.text}</Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-right">
-                    <Button variant="link">Submit</Button>
-                </Card.Footer>
-            </Card>
-        );
-    }
+export default function SubsectionCard(props) {
+    return (
+        <Card border="primary" className="subsection">
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.text}</Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-right">
+                <Button variant="link">Submit</Button>
+            </Card.Footer>
+        </Card>
+    );
 }
