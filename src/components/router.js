2854747fc54a1fcafcbbbab924ca04f5e0372651
kostya05983 <kostya05983@mail.ru>
2019-05-06 15:09:00 +0700

    [ADD] new service of auth

--- src/components/router.js
+++ src/components/router.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
 } from "react-router-dom";
+import PrivateRoute from 'PrivateRouter'
 
 //Pages
 import Navigation from '../pages/Navigation';
 import Schemes from '../pages/Schemes';
 import EmptyPage from '../pages/NotFound';
 
-export default function Routers(){
+export default function Routers() {
     return (
         <Router>
-          <Switch>
-            <Route exact path='/' component={Navigation}/>
-            <Route exact path='/schemes' component={Schemes}/>
-            <Route path='/404' component={EmptyPage}/>
-            <Redirect to='/404'/>
-          </Switch>
+            <Switch>
+                <Route exact path='/' component={Navigation}/>
+                <PrivateRoute exact path='/schemes' component={Schemes}/>
+                <PrivateRoute path='/404' component={EmptyPage}/>
+                <Route path='/login' component={Login}/>
+                <Redirect to='/404'/>
+            </Switch>
         </Router>
     );
-  }
+}
