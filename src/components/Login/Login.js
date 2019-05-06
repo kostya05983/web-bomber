9cd12db4fda6d3383c721b3b7ce87b336a67187d
kostya05983 <kostya05983@mail.ru>
2019-05-07 00:03:22 +0700

    [ADD] login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,141 @@
+import React, {Component} from 'react';
+import './Login.css';
+import {GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL, ACCESS_TOKEN} from '../../constants';
+import {login} from '../../util/APIUtils';
+import {Link, Redirect} from 'react-router-dom'
+import fbLogo from '../../assets/fb-logo.png';
+import googleLogo from '../../assets/google-logo.png';
+import githubLogo from '../../assets/github-logo.png';
+import Alert from 'react-s-alert';
+import {connect} from 'react-redux'
+import autoBind from "react-autobind";
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        autoBind(this)
+    }
+
+    componentDidMount() {
+        // If the OAuth2 login encounters an error, the user is redirected to the /login page with an error.
+        // Here we display the error and then remove the error query parameter from the location.
+        if (this.props.location.state && this.props.location.state.error) {
+            setTimeout(() => {
+                Alert.error(this.props.location.state.error, {
+                    timeout: 5000
+                });
+                this.props.history.replace({
+                    pathname: this.props.location.pathname,
+                    state: {}
+                });
+            }, 100);
+        }
+    }
+
+    changeOnSignUp() {
+        this.props.dispatch(headersActions.updateHeader("SignUp"))
+    }
+
+    render() {
+        if (this.props.authenticated) {
+            console.log(this.props);
+            return <Redirect
+                to={{
+                    pathname: "/projects",
+                    state: {from: this.props.location}
+                }}/>;
+        }
+
+        return (
+            <div className="login-container">
+                <div className="login-content">
+                    <h1 className="login-title">Login to MEGADO</h1>
+                    <SocialLogin/>
+                    <div className="or-separator">
+                        <span className="or-text">OR</span>
+                    </div>
+                    <LoginForm {...this.props} />
+                    <span className="signup-link" onClick={this.changeOnSignUp}>New user? <Link to="/signup"
+                                                                                                onClick={this.changeOnSignUp}>Sign up!</Link></span>
+                </div>
+            </div>
+        );
+    }
+}
+
+class SocialLogin extends Component {
+    render() {
+        return (
+            <div className="social-login">
+                <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
+                    <img src={googleLogo} alt="Google"/> Log in with Google</a>
+                <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
+                    <img src={fbLogo} alt="Facebook"/> Log in with Facebook</a>
+                <a className="btn btn-block social-btn github" href={GITHUB_AUTH_URL}>
+                    <img src={githubLogo} alt="Github"/> Log in with Github</a>
+            </div>
+        );
+    }
+}
+
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName]: inputValue
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const loginRequest = Object.assign({}, this.state);
+
+        login(loginRequest)
+            .then(response => {
+                localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+
+                Alert.success("You're successfully logged in!");
+                this.props.history.push("/projects");
+            }).catch(error => {
+            Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-item">
+                    <input type="email" name="email"
+                           className="form-control" placeholder="Email"
+                           value={this.state.email} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <input type="password" name="password"
+                           className="form-control" placeholder="Password"
+                           value={this.state.password} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <button type="submit" className="btn btn-block btn-primary">Login</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default connect()(Login)
