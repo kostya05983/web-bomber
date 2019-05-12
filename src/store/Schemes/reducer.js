bdfe76760d89484278546d49e54c96672099740a
kostya05983 <kostya05983@mail.ru>
2019-05-12 15:05:04 +0700

    [ADD] add store for scheme, update

--- src/store/Schemes/reducer.js
+++ src/store/Schemes/reducer.js
@@ -1,17 +1,26 @@
-import {TOGGLE_MODAL} from "./constants";
+import {ADD_SCHEME, FETCHED_SCHEMES, REMOVE_SCHEME, TOGGLE_MODAL, UPDATE_SCHEME} from "./constants";
 
 const initState = {
     loading: true,
-    isOpen: false
+    isOpen: false,
+    schemes: []
 };
 
-
-export default function reducer(state = initStatem, action) {
-    const {type, payload} = action;
-
-    switch (type) {
+export default function reducer(state = initState, action) {
+    switch (action.type) {
         case TOGGLE_MODAL:
             return {...state, isOpen: !state.isOpen};
+        case FETCHED_SCHEMES:
+            return {...state, schemes: action.payload};
+        case ADD_SCHEME:
+            console.log("Add scheme", action.payload);
+            return state;
+        case UPDATE_SCHEME:
+            console.log("UPDATE scheme", action.payload);
+            return state;
+        case REMOVE_SCHEME:
+            console.log("Remove scheme", action.payload);
+            return state;
         default:
             return state
     }
