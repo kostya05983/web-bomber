3bd2a4944877f44d6af43f72b4409672bce9077b
kostya05983 <kostya05983@mail.ru>
2019-12-15 02:40:21 +0700

    Update RestSchemaService.

---
+++ src/services/rest/RestSchemaService.js
@@ -0,0 +1,43 @@
+import {API_BASE_URL} from "../constants";
+import request from '../Request'
+
+const REST_SCHEMA_URL = API_BASE_URL + "/bomber/bomber-api/schema/v1";
+
+class RestSchemaService {
+    async createSchema(createSchemaRequest) {
+        return await request({
+            url: REST_SCHEMA_URL,
+            method: 'POST',
+            body: createSchemaRequest
+        })
+    }
+
+    async updateSchema(id, updateSchemaRequuest) {
+        return await request({
+            url: REST_SCHEMA_URL + "/" + id,
+            method: "PATCH",
+            body: updateSchemaRequuest
+        })
+    }
+
+    async getSchema(id) {
+        return await request({
+            url: REST_SCHEMA_URL + "/" + id,
+            method: "GET"
+        })
+    }
+
+    async getSchemes(offset, limit) {
+        return await request({
+            url: REST_SCHEMA_URL + "?offset=" + offset + "&limit=" + limit,
+            method: "GET"
+        })
+    }
+
+    async deleteScheme(id) {
+        return await request({
+            url: REST_SCHEMA_URL + "/" + id,
+            method: "DELETE"
+        })
+    }
+}
