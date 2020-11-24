2287807e4e3cba5de6a118df710acb4ffc81c259
kostya05983 <kostya05983@mail.ru>
2020-11-24 22:44:49 +0700

    Add form service

--- src/services/FormService.js
+++ src/services/FormService.js
@@ -1,14 +1,38 @@
 import {API_BASE_URL} from "./constants";
 import request from "./Request";
 
-const REST_SCRIPT_URL = API_BASE_URL+"/bomber/bomber-api/forms";
+const REST_SCRIPT_URL = API_BASE_URL + "/bomber/bomber-api/v1/test-forms";
 
 class FormsService {
-    async createForm(createFormRequest) {
+    async create(createFormRequest) {
         return await request({
             url: REST_SCRIPT_URL,
             method: "POST",
             body: createFormRequest
         })
     }
+
+    async update(id, updateFormRequest) {
+        return await request({
+            url: REST_SCRIPT_URL + "/" + id,
+            method: "PATCH",
+            body: updateFormRequest
+        })
+    }
+
+    async run(id) {
+        return await request({
+            url: REST_SCRIPT_URL + "/" + id + "/run",
+            method: "POST"
+        })
+    }
+
+    async delete(id) {
+        return await request({
+            url: REST_SCRIPT_URL + "/" + id,
+            method: "DELETE"
+        })
+    }
 }
+
+export default new FormsService()
